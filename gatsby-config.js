18555f48d51edc82536a69ea0705b4c84e679ca5
Jakub Birnbach <kubabirnbach@gmail.com>
2020-09-13 01:55:58 +0200

    env config changed 9

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Chyba Ty Tattoo`,
