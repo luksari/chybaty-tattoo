6a2f3ecd904e1cd30bf06be5ff8ff5b1da2a1be7
Jakub Birnbach <kubabirnbach@gmail.com>
2020-09-13 13:01:01 +0200

    env config changed 19

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,8 @@ const IndexPage = ({ location }) => {
     styles: googleMapStyles
   }
 
+  console.log("Index", process.env)
+
   return (
     <>
       <Helmet>
@@ -35,7 +37,7 @@ const IndexPage = ({ location }) => {
         <Artists />
         <GoogleMapContainer>
           <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
+            bootstrapURLKeys={{ key: process.env.GATSBY_GOOGLE_MAP_API_KEY }}
             defaultCenter={{
               lat: 50.258253,
               lng: 19.01462,
