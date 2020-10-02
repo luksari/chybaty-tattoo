37ce753e5172acdc85420bb95a106e5f32c2eae6
Jakub Birnbach <kubabirnbach@gmail.com>
2020-10-02 11:34:55 +0200

    fix: useWidth undefined error fix

--- src/helpers/useWindowWidth.js
+++ src/helpers/useWindowWidth.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react"
 
 export function useWindowWidth() {
-  const isBrowser = window !== 'undefined'
-  const [width, setWidth] = useState(isBrowser ? window.innerWidth : 0)
+  if (typeof window !== `undefined`) {
+    const isBrowser = window !== "undefined"
+    const [width, setWidth] = useState(isBrowser ? window.innerWidth : 0)
 
-  useEffect(() => {
-    if (!isBrowser) {
-      return;
-    }
-    const handleResize = () => setWidth(window.innerWidth)
-    window.addEventListener("resize", handleResize)
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [isBrowser])
+    useEffect(() => {
+      if (!isBrowser) {
+        return
+      }
+      const handleResize = () => setWidth(window.innerWidth)
+      window.addEventListener("resize", handleResize)
+      return () => {
+        window.removeEventListener("resize", handleResize)
+      }
+    }, [isBrowser])
 
-  return width
+    return width
+  }
 }
